c3937ac711168f189db0b013676e737f4c77290d
Yan-rlathotep <campo.yannis@gmail.com>
2020-05-25 21:26:57 +0200

    Display login form

--- frontend/src/pages/Signup/Signup.component.tsx
+++ frontend/src/pages/Signup/Signup.component.tsx
@@ -2,11 +2,18 @@ import React, { Dispatch, SetStateAction } from 'react';
 import SignupForm from 'components/forms/SignupForm';
 import './Signup.scss';
 
-const Signup = ({ setShouldShowSignup }: { setShouldShowSignup: Dispatch<SetStateAction<boolean>> }) => (
+const Signup = ({
+  setShouldShowSignup,
+}: {
+  setShouldShowSignup: Dispatch<SetStateAction<boolean>>;
+}) => (
   <div className="signup-container">
     <h1 className="signup-title">You aren't logged in!</h1>
     <p className="signup-subtitle">You bad turtle</p>
-    <SignupForm setShouldShowSignup={setShouldShowSignup} />
+    <div className="forms-container">
+      <SignupForm setShouldShowSignup={setShouldShowSignup} />
+      <SignupForm setShouldShowSignup={setShouldShowSignup} isLogin />
+    </div>
   </div>
 );
 
