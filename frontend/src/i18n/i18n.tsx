2f296ddd5fba359b03482d24da4535834e162ded
Yan-rlathotep <campo.yannis@gmail.com>
2020-05-26 21:08:36 +0200

    Set up i18n

---
+++ frontend/src/i18n/i18n.tsx
@@ -0,0 +1,20 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './translations/en';
+
+const resources = {
+  en: { translation: en },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+
+  keySeparator: '.',
+
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
---
+++ frontend/src/i18n/translations/en.tsx
@@ -0,0 +1,21 @@
+export default {
+  pages: {
+    signup: {
+      title: "You aren't logged in!",
+      subtitle: 'You bad turtle',
+      login: 'Log in',
+      signup: 'Sign up',
+      username: 'Username',
+      password: 'Password',
+      submit: 'Submit Query',
+      usernameAlreadyTaken: 'Error: this username is already taken',
+      invalidCredentials: 'Error: username or password is invalid',
+    },
+    home: {
+      welcome: 'Welcome, {{username}}! You finally logged in',
+      subtitle: 'You good turtle',
+      enjoy: 'Now enjoy staring at a blank page \\(°-° )/',
+      clearButton: 'Clear localStorage',
+    },
+  },
+};
