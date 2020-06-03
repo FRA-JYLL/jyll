d6329869bf8fec08fba00b73da42930d36db9f0b
Yan-rlathotep <campo.yannis@gmail.com>
2020-06-03 23:05:10 +0200

    Add Modal component

---
+++ frontend/src/components/modals/Modal.component.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { CSSTransition } from 'react-transition-group';
+import './Modal.scss';
+
+interface Props {
+  isOpen: boolean;
+  closeModal: () => void;
+  onClosed?: () => void;
+}
+
+const Modal = ({ isOpen, closeModal, onClosed, children }: React.PropsWithChildren<Props>) => (
+  <>
+    <CSSTransition
+      in={isOpen}
+      timeout={{ enter: 500, exit: 200 }}
+      classNames={'modal'}
+      onExited={onClosed}
+      mountOnEnter
+      unmountOnExit
+    >
+      <div className="modal-container">{children}</div>
+    </CSSTransition>
+    <CSSTransition
+      in={isOpen}
+      timeout={{ enter: 500, exit: 200 }}
+      classNames={'modal'}
+      mountOnEnter
+      unmountOnExit
+    >
+      <div className="modal-overlay" onClick={closeModal}></div>
+    </CSSTransition>
+  </>
+);
+
+export default Modal;
