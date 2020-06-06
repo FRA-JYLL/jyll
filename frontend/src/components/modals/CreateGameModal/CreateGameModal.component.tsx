b35ac097f9119c49d2247929550133e4c08be552
Yan-rlathotep <campo.yannis@gmail.com>
2020-06-06 21:27:20 +0200

    Minor fixes

--- frontend/src/components/modals/CreateGameModal/CreateGameModal.component.tsx
+++ frontend/src/components/modals/CreateGameModal/CreateGameModal.component.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import './CreateGameModal.scss';
-import { Modal } from '../index';
+import { Modal } from 'components/modals';
 import { ContainerProps } from './CreateGameModal.container';
 
 interface Props extends ContainerProps {
@@ -24,48 +24,41 @@ const CreateGameModal = ({ isOpen, closeModal, createGame }: Props) => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    createGame(
-      gameName !== '' ? gameName : undefined,
-      gamePassword !== '' ? gamePassword : undefined
-    );
+    createGame(gameName || undefined, gamePassword || undefined);
 
     closeModalAndResetFields();
   };
 
   return (
     <Modal isOpen={isOpen} closeModal={closeModalAndResetFields}>
-      <div className="createGameModal-container">
-        <form className="createGameModal-form" onSubmit={handleSubmit}>
-          <p className="createGameModal-label">
-            {t('pages.gameSelection.createGameModal.gameName')}
-          </p>
-          <input
-            className="createGameModal-input"
-            type="text"
-            onChange={(e) => setGameName(e.target.value)}
-          />
-          <p className="createGameModal-label">
-            {t('pages.gameSelection.createGameModal.gamePassword')}
-          </p>
-          <input
-            className="createGameModal-input"
-            type="text"
-            onChange={(e) => setGamePassword(e.target.value)}
-          />
-          <div className="createGameModal-button-container">
-            <button
-              className="createGameModal-button"
-              type={'button'}
-              onClick={closeModalAndResetFields}
-            >
-              {t('pages.gameSelection.createGameModal.cancel')}
-            </button>
-            <button className="createGameModal-button" type="submit">
-              {t('pages.gameSelection.createGameModal.submit')}
-            </button>
-          </div>
-        </form>
-      </div>
+      <form className="createGameModal-form" onSubmit={handleSubmit}>
+        <p className="createGameModal-label">{t('pages.gameSelection.createGameModal.gameName')}</p>
+        <input
+          className="createGameModal-input"
+          type="text"
+          onChange={(e) => setGameName(e.target.value)}
+        />
+        <p className="createGameModal-label">
+          {t('pages.gameSelection.createGameModal.gamePassword')}
+        </p>
+        <input
+          className="createGameModal-input"
+          type="text"
+          onChange={(e) => setGamePassword(e.target.value)}
+        />
+        <div className="createGameModal-button-container">
+          <button
+            className="createGameModal-button"
+            type={'button'}
+            onClick={closeModalAndResetFields}
+          >
+            {t('pages.gameSelection.createGameModal.cancel')}
+          </button>
+          <button className="createGameModal-button" type="submit">
+            {t('pages.gameSelection.createGameModal.submit')}
+          </button>
+        </div>
+      </form>
     </Modal>
   );
 };
