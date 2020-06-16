78d2ece6b1eceaf31a7a206975e32347a12df51c
BatyLeo <38216871+BatyLeo@users.noreply.github.com>
2020-06-16 21:00:24 +0200

    Serializer enhancements (#36)
    * add username in PlayerSerializer
    * return full players in `game/<game_id>/players/` endpoint response
    * remove has_password from read only fields in GameSerializer

--- backend/game/serializers.py
+++ backend/game/serializers.py
@@ -15,7 +15,7 @@ class GameSerializer(serializers.ModelSerializer):
             "creation_date",
             "has_password",
         ]
-        read_only_fields = ["is_pending", "creation_date", "has_password"]
+        read_only_fields = ["is_pending", "creation_date"]
         extra_kwargs = {"password": {"write_only": True}, "name": {"required": False}}
 
     def get_has_password(self, game):
@@ -23,7 +23,12 @@ class GameSerializer(serializers.ModelSerializer):
 
 
 class PlayerSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField(method_name="get_username")
+
     class Meta:
         model = Player
-        fields = ["id", "is_admin", "user", "game", "is_ready"]
+        fields = ["id", "is_admin", "user", "game", "is_ready", "username"]
         read_only_fields = ["is_admin", "user", "game"]
+
+    def get_username(self, player):
+        return player.user.username
